0d6d724b8cf76fae0c8f6c7c55a9f2958dc14852
Nick Pope <nick.pope@flightdataservices.com>
2012-02-29 11:37:54 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="soapbox",
-    version="0.2.0",
-    author="Damian Powazka",
-    author_email="dpowazka@gmail.com",
-    url="http://code.google.com/p/soapbox/",
-    description="Python Django-like SOAP library",
-    download_url="",
-    license="New BSD",
-    install_requires=['lxml','jinja2'],
-    packages=["soapbox"],
+    name='soapbox',
+    version='0.2.0',
+    author='Damian Powazka',
+    author_email='dpowazka@gmail.com',
+    url='http://code.google.com/p/soapbox/',
+    description='Python Django-like SOAP library',
+    download_url='',
+    license='New BSD',
+    install_requires=['argparse', 'jinja2', 'lxml'],
+    packages=['soapbox'],
     entry_points={
         'console_scripts': [
             'py2wsdl=soapbox.py2wsdl:main',
@@ -19,12 +19,12 @@ setup(
             'xsd2py=soapbox.xsd2py:main',
         ],
     },
-    platforms="Python 2.6 and later.",
+    platforms='Python 2.6 and later.',
     classifiers=[
-        "Development Status :: 2 - Beta",
-        "License :: OSI Approved :: New BSD",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Intended Audience :: Science/Research",
-        ]
-    )
+        'Development Status :: 2 - Beta',
+        'License :: OSI Approved :: New BSD',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Intended Audience :: Science/Research',
+    ],
+)
