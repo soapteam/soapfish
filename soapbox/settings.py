20bc330b1f3d2d88c9367a2857d7a3a4fcaca39b
Nick Pope <nick.pope@flightdataservices.com>
2012-03-08 17:52:32 +0000

    Added missing file

---
+++ soapbox/settings.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+################################################################################
+
+'''
+'''
+
+################################################################################
+# Imports
+
+
+# Location of the CA certificates to be used by httplib2:
+# Note: This is the system-wide default location on Ubuntu.
+CA_CERTIFICATE_FILE = '/etc/ssl/certs/ca-certificates.crt'
+
+
+################################################################################
+# Flags
+
+
+# For deciding whether to validate against schemas when parsing:
+VALIDATE_ON_PARSE = True
+
+
+################################################################################
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4 Flags
