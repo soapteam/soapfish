0c3a55a8256448575dd2c7ce076f0fe67398f084
Martin Wimpress <martin.wimpress@flightdataservices.com>
2013-04-12 18:04:04 +0100

    Disable rst2pdf.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,13 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 'rst2pdf.pdfbuilder']
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.doctest',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.pngmath',
+              'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
