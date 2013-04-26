d4d96f585818e9db2291a6b9423495fc5003a417
Martin Wimpress <martin.wimpress@flightdataservices.com>
2013-04-26 16:35:59 +0100

    Removed everything related to rst2pdf. Consolidated Sphinx configuration.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Soapbox documentation build configuration file, created by
+# Documentation build configuration file, created by
 # sphinx-quickstart on Tue Mar 20 17:20:46 2012.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -11,7 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
 import soapbox as pkg
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -26,12 +27,13 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.coverage',              
               'sphinx.ext.doctest',
               'sphinx.ext.intersphinx',
-              'sphinx.ext.todo',
-              'sphinx.ext.coverage',
               'sphinx.ext.pngmath',
+              'sphinx.ext.todo',
               'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -178,7 +180,7 @@ htmlhelp_basename = pkg.__packagename__ + 'doc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
@@ -190,13 +192,13 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Soapbox.tex', u'Soapbox Documentation',
-   u'Flight Data Services, Ltd', 'manual'),
+  ('index', pkg.__packagename__ + '.tex', pkg.__packagename__ + ' Documentation',
+   pkg.__author__, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'fds-icon.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
@@ -234,8 +236,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Soapbox', u'Soapbox Documentation',
-   u'Flight Data Services, Ltd', 'Soapbox', 'One line description of project.',
+  ('index', pkg.__packagename__, pkg.__packagename__ + ' Documentation',
+   pkg.__author__, pkg.__packagename__, pkg.__description__,
    'Miscellaneous'),
 ]
 
@@ -252,101 +254,3 @@ todo_include_todos = True
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
-
-# -- Options for PDF output --------------------------------------------------
-
-# Grouping the document tree into PDF files. List of tuples
-# (source start file, target name, title, author, options).
-#
-# If there is more than one author, separate them with \\.
-# For example: r'Guido van Rossum\\Fred L. Drake, Jr., editor'
-#
-# The options element is a dictionary that lets you override 
-# this config per-document.
-# For example, 
-# ('index', u'MyProject', u'My Project', u'Author Name', 
-#  dict(pdf_compressed = True))
-# would mean that specific document would be compressed
-# regardless of the global pdf_compressed setting.
-
-pdf_documents = [ 
-    ('index', pkg.__packagename__, pkg.__packagename__, pkg.__author__),
-]
-
-# A comma-separated list of custom stylesheets. Example:
-pdf_stylesheets = ['tenpoint', 'autumn', 'a4']
-
-# A list of folders to search for stylesheets. Example:
-pdf_style_path = ['.', '_styles']
-
-# Create a compressed PDF
-# Use True/False or 1/0
-# Example: compressed=True
-#pdf_compressed = False
-
-# A colon-separated list of folders to search for fonts. Example:
-# pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
-
-# Language to be used for hyphenation support
-pdf_language = "en_GB"
-
-# Mode for literal blocks wider than the frame. Can be
-# overflow, shrink or truncate
-pdf_fit_mode = "shrink"
-
-# Section level that forces a break page.
-# For example: 1 means top-level sections start in a new page
-# 0 means disabled
-pdf_break_level = 1
-
-# When a section starts in a new page, force it to be 'even', 'odd',
-# or just use 'any'
-pdf_breakside = 'any'
-
-# Insert footnotes where they are defined instead of 
-# at the end.
-#pdf_inline_footnotes = True
-
-# verbosity level. 0 1 or 2
-#pdf_verbosity = 0
-
-# If false, no index is generated.
-#pdf_use_index = True
-
-# If false, no modindex is generated.
-#pdf_use_modindex = True
-
-# If false, no coverpage is generated.
-#pdf_use_coverpage = True
-
-# Name of the cover page template to use
-pdf_cover_template = 'sphinxcover.tmpl'
-
-# Documents to append as an appendix to all manuals.    
-#pdf_appendices = []
-
-# Enable experimental feature to split table cells. Use it
-# if you get "DelayedTable too big" errors
-#pdf_splittables = False
-
-# Set the default DPI for images
-#pdf_default_dpi = 72
-
-# Enable rst2pdf extension modules (default is only vectorpdf)
-# you need vectorpdf if you want to use sphinx's graphviz support
-#pdf_extensions = ['vectorpdf']
-
-# Page template name for "regular" pages
-#pdf_page_template = 'cutePage'
-
-# Show Table Of Contents at the beginning?
-#pdf_use_toc = True
-
-# How many levels deep should the table of contents be?
-pdf_toc_depth = 3
-
-# Add section number to section references
-pdf_use_numbered_links = False
-
-# Background images fitting mode
-pdf_fit_background_mode = 'scale'
