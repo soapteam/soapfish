soapteam/soapfish

commit ab6be5a485569ddf7042497d920cc5a1843b2fa0

Django dispatcher extending SOAPDispatcher

soapbox/soap_dispatch.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 
+import logging
 from lxml import etree
 import six
 
@@ -13,6 +14,9 @@ from .soap import SOAPError
 basestring = six.string_types
 
 __all__ = ['SOAPDispatcher', 'SoapboxRequest']
+
+logger = logging.getLogger(__name__)
+
 
 class DjangoCompatHeaders(object):
     def __init__(self, headers):
@@ -153,7 +157,10 @@ class SOAPDispatcher(object):
             return self.error_response(SOAP.Code.CLIENT, input_validation.errors)
         validated_input = input_validation.validated_document
 
-        soap_response_message, is_error = self._call_handler(soapbox_request, handler, validated_input)
-        # if not soap_response_message -> server error
-        # SOAP.get_error_response(SOAP.Code.SERVER, str(e))
+        try:
+            soap_response_message, is_error = self._call_handler(soapbox_request, handler, validated_input)
+        except Exception as ex:
+            logger.exception('Error during SOAP handling')
+            return self.error_response(SOAP.Code.SERVER, (str(ex),))
+
         return self.response(soap_response_message, is_error=is_error)

soapbox/ext/django/dispatch.py
@@ -1,0 +1,37 @@
+from soapbox.soap_dispatch import SOAPDispatcher
+from soapbox import py2wsdl
+
+import django
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+
+class DjangoSoapboxRequest(object):
+    def __init__(self, request):
+        self.request = request
+        self.method = request.method
+        self.META = request.META
+
+
+class DjangoSOAPDispatcher(SOAPDispatcher):
+    def __init__(self, service):
+        super(DjangoSOAPDispatcher, self).__init__(service)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request):
+        if request.method == 'GET' and 'wsdl' in request.GET:
+            wsdl = py2wsdl.generate_wsdl(self.service)
+            return HttpResponse(wsdl, mimetype='text/xml')
+
+        soapbox_request = DjangoSoapboxRequest(request)
+        if django.VERSION < (1, 4):
+            body_contents = request.raw_post_data
+        else:
+            body_contents = request.body
+        soap_response = super(DjangoSOAPDispatcher, self).dispatch(soapbox_request, body_contents)
+        django_response =  HttpResponse(
+            soap_response.message,
+            content_type=soap_response.content_type)
+        django_response.status_code = soap_response.status
+        return django_response

soapbox/py2wsdl.py
@@ -7,6 +7,7 @@ import argparse
 import imp
 import logging
 import textwrap
+import six
 
 from lxml import etree
 
@@ -16,6 +17,7 @@ from .soap import SOAP_HTTP_Transport
 from .utils import uncapitalize
 from .wsdl import get_wsdl_classes
 
+basestring = six.string_types
 
 logger = logging.getLogger('soapbox')
 

soapbox/templates/wsdl
@@ -91,8 +91,8 @@ def {{ operation.name }}(request, {{ inputMessage.part.element|remove_namespace 
 {# [blank line] #}
 {# [blank line] #}
 # Uncomment these lines to turn on dispatching:
-#from django.views.decorators.csrf import csrf_exempt
-#dispatch = csrf_exempt(soap.get_django_dispatch({{ port.name }}_SERVICE))
+#from soapbox.ext.django.dispatch import DjangoSOAPDispatcher
+#dispatcher = DjangoSOAPDispatcher({{ port.name }}_SERVICE))
 {# [blank line] #}
 # Put these lines in the urls.py file of your Django project/application:
 #urlpatterns += patterns('',

commit 1c0851ba61b3d51dae0caf8b9898bb792fbdfcb5

remove the exception handler from SOAPDispatcher (introduced in ab6be5a4) because the simple solution breaks debugging middlewares

soapbox/soap_dispatch.py
@@ -157,10 +157,11 @@ class SOAPDispatcher(object):
             return self.error_response(SOAP.Code.CLIENT, input_validation.errors)
         validated_input = input_validation.validated_document
 
-        try:
-            soap_response_message, is_error = self._call_handler(soapbox_request, handler, validated_input)
-        except Exception as ex:
-            logger.exception('Error during SOAP handling')
-            return self.error_response(SOAP.Code.SERVER, (str(ex),))
+        # LATER: the dispatcher should be able to also catch arbitrary
+        # exceptions from the handler. At the same time the caller should be
+        # able to install custom hooks for these exceptions (e.g. custom
+        # traceback logging) and we should support popular debugging middlewares
+        # somehow.
+        soap_response_message, is_error = self._call_handler(soapbox_request, handler, validated_input)
 
         return self.response(soap_response_message, is_error=is_error)