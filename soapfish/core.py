6844134d8d4a3cf02427b38cc239cb2c459248fe
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2014-11-20 22:32:50 +0100

    tiny refactoring: re-order classes and instance variables for better coherency

--- soapfish/core.py
+++ soapfish/core.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-class SOAPResponse(object):
-    def __init__(self, soap_body, soap_header=None, http_status_code=200, http_content=None,
-                 http_headers=None):
-        self.soap_body = soap_body
-        self.soap_header = soap_header
-        self.http_status_code = 200
-        self.http_content = http_content
-        self.http_headers = {} if http_headers is None else http_headers
-
-
-class SOAPRequest(object):
-    def __init__(self, environ, http_content):
-        self.environ = environ
-        self.http_content = http_content
-        self.soap_header = None
-        self.soap_body = None
-        self.dispatcher = None
-        self.method = None
-
+__all__ = ['SOAPError', 'SOAPRequest', 'SOAPResponse']
 
 class SOAPError(Exception):
     def __init__(self, code, message, actor=None):
@@ -30,3 +12,23 @@ class SOAPError(Exception):
 
     def __str__(self):
         return "(%s) %s, actor=%s" % (self.code, self.message, self.actor)
+
+
+class SOAPResponse(object):
+    def __init__(self, soap_body, soap_header=None, http_status_code=200, http_content=None,
+                 http_headers=None):
+        self.soap_header = soap_header
+        self.soap_body = soap_body
+        self.http_status_code = 200
+        self.http_headers = {} if http_headers is None else http_headers
+        self.http_content = http_content
+
+
+class SOAPRequest(object):
+    def __init__(self, environ, http_content):
+        self.environ = environ
+        self.http_content = http_content
+        self.soap_header = None
+        self.soap_body = None
+        self.dispatcher = None
+        self.method = None
