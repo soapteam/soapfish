9e9cb6b53add22e4dd89038158aae0267796f960
Iuri de Silvio <iurisilvio@gmail.com>
2015-11-28 16:34:06 -0200

    Schema xsd:import from same namespace. Fix #49

--- soapfish/xsd2py.py
+++ soapfish/xsd2py.py
@@ -54,50 +54,61 @@ def get_rendering_environment():
     return env
 
 
-def resolve_import(xsdimport, known_namespaces, parent_namespace, cwd):
+def resolve_import(xsdimport, known_files, parent_namespace, cwd):
     location = os.path.join(cwd, xsdimport.schemaLocation)
     cwd = os.path.dirname(location)
     logger.info('Generating code for XSD import \'%s\'...' % location)
     xml = open_document(location)
     xmlelement = etree.fromstring(xml)
-    return generate_code_from_xsd(xmlelement, known_namespaces, location,
+    return generate_code_from_xsd(xmlelement, known_files, location,
                                   parent_namespace, encoding=None, cwd=cwd)
 
 
-def schema_name(namespace):
+def schema_name(schema, location=None):
+    if not location:
+        try:
+            location = schema.schemaLocation
+        except AttributeError:
+            location = schema.targetNamespace
+
+    try:
+        location = location.encode()
+    except UnicodeEncodeError:
+        pass
     # we don't have any cryptographic requirements here and md5 is faster than
     # sha512 so there is no harm using an outdated algorithm.
-    return hashlib.md5(namespace.encode()).hexdigest()[0:5]
+    return hashlib.md5(location).hexdigest()[0:5]
 
 
-def generate_code_from_xsd(xmlelement, known_namespaces=None, location=None,
+def generate_code_from_xsd(xmlelement, known_files=None, location=None,
                            parent_namespace=None, encoding='utf8', cwd=None):
-    if known_namespaces is None:
-        known_namespaces = []
+    if known_files is None:
+        known_files = []
     xsd_namespace = find_xsd_namespaces(xmlelement.nsmap)
 
     schema = Schema.parse_xmlelement(xmlelement)
 
     # Skip if this schema has already been included:
-    if schema.targetNamespace in known_namespaces:
+    if schema.targetNamespace in known_files:
         return ''
 
-    schema_code = schema_to_py(schema, xsd_namespace, known_namespaces,
+    schema_code = schema_to_py(schema, xsd_namespace, known_files,
                                location, cwd=cwd)
     if encoding is None:
         return schema_code
     return schema_code.encode(encoding)
 
 
-def schema_to_py(schema, xsd_namespace, known_namespaces=None, location=None,
+def schema_to_py(schema, xsd_namespace, known_files=None, location=None,
                  parent_namespace=None, cwd=None):
-    if known_namespaces is None:
-        known_namespaces = []
-    known_namespaces.append(schema.targetNamespace)
+    if known_files is None:
+        known_files = []
+    if location:
+        known_files.append(location)
 
     env = get_rendering_environment()
     env.filters['type'] = get_get_type(xsd_namespace)
-    env.globals['known_namespaces'] = known_namespaces
+    env.globals['known_files'] = known_files
     env.globals['location'] = location
 
     tpl = env.get_template('xsd')
