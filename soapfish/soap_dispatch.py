11579630956c5730c2735d25223873c5e2f8f39d
Nick Pope <nick.pope@flightdataservices.com>
2016-03-19 20:39:39 +0000

    soap: properly parse query strings in dispatch.
    License: BSD

--- soapfish/soap_dispatch.py
+++ soapfish/soap_dispatch.py
@@ -221,10 +221,9 @@ class SOAPDispatcher(object):
 
     def handle_xsd_request(self, request):
         qs = request.environ.get('QUERY_STRING')
-        xsd_filename = qs[qs.index('xsd=') + 4:]
-        if '&' in xsd_filename:
-            xsd_filename, _ = xsd_filename.split('&', 1)
-        return SOAPResponse('xsd', http_content=self.xsds[xsd_filename], http_headers={'Content-Type': 'text/xml'})
+        qs = six.moves.urllib.parse.parse_qs(qs)
+        xsd = self.xsds[qs.get('xsd')]
+        return SOAPResponse('xsd', http_content=xsd, http_headers={'Content-Type': 'text/xml'})
 
     def _generate_xsds(self, schema, _generated=None):
         if _generated is None:
