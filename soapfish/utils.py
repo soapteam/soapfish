soapteam/soapfish

commit 9e9cb6b53add22e4dd89038158aae0267796f960

Schema xsd:import from same namespace. Fix #49

soapfish/xsd2py.py
@@ -54,50 +54,61 @@ def get_rendering_environment():
     return env
 
 
-def resolve_import(xsdimport, known_namespaces, parent_namespace, cwd):
+def resolve_import(xsdimport, known_files, parent_namespace, cwd):
     location = os.path.join(cwd, xsdimport.schemaLocation)
     cwd = os.path.dirname(location)
     logger.info('Generating code for XSD import \'%s\'...' % location)
     xml = open_document(location)
     xmlelement = etree.fromstring(xml)
-    return generate_code_from_xsd(xmlelement, known_namespaces, location,
+    return generate_code_from_xsd(xmlelement, known_files, location,
                                   parent_namespace, encoding=None, cwd=cwd)
 
 
-def schema_name(namespace):
+def schema_name(schema, location=None):
+    if not location:
+        try:
+            location = schema.schemaLocation
+        except AttributeError:
+            location = schema.targetNamespace
+
+    try:
+        location = location.encode()
+    except UnicodeEncodeError:
+        pass
     # we don't have any cryptographic requirements here and md5 is faster than
     # sha512 so there is no harm using an outdated algorithm.
-    return hashlib.md5(namespace.encode()).hexdigest()[0:5]
+    return hashlib.md5(location).hexdigest()[0:5]
 
 
-def generate_code_from_xsd(xmlelement, known_namespaces=None, location=None,
+def generate_code_from_xsd(xmlelement, known_files=None, location=None,
                            parent_namespace=None, encoding='utf8', cwd=None):
-    if known_namespaces is None:
-        known_namespaces = []
+    if known_files is None:
+        known_files = []
     xsd_namespace = find_xsd_namespaces(xmlelement.nsmap)
 
     schema = Schema.parse_xmlelement(xmlelement)
 
     # Skip if this schema has already been included:
-    if schema.targetNamespace in known_namespaces:
+    if schema.targetNamespace in known_files:
         return ''
 
-    schema_code = schema_to_py(schema, xsd_namespace, known_namespaces,
+    schema_code = schema_to_py(schema, xsd_namespace, known_files,
                                location, cwd=cwd)
     if encoding is None:
         return schema_code
     return schema_code.encode(encoding)
 
 
-def schema_to_py(schema, xsd_namespace, known_namespaces=None, location=None,
+def schema_to_py(schema, xsd_namespace, known_files=None, location=None,
                  parent_namespace=None, cwd=None):
-    if known_namespaces is None:
-        known_namespaces = []
-    known_namespaces.append(schema.targetNamespace)
+    if known_files is None:
+        known_files = []
+    if location:
+        known_files.append(location)
 
     env = get_rendering_environment()
     env.filters['type'] = get_get_type(xsd_namespace)
-    env.globals['known_namespaces'] = known_namespaces
+    env.globals['known_files'] = known_files
     env.globals['location'] = location
 
     tpl = env.get_template('xsd')

soapfish/templates/wsdl
@@ -59,7 +59,7 @@ def {{ operation.name }}(request, {{ inputMessage.part.element|remove_namespace 
 {%- set outputMessageHeaders = operation.get_OutputMessageHeaders() -%}
 {%- if inputMessageHeaders %}
 class {{ inputMessage.name }}Headers(BaseHeader):
-    SCHEMA = Schema_{{ schema_name(definitions.types.schema.targetNamespace) }}
+    SCHEMA = Schema_{{ schema_name(definitions.types.schema) }}
     {%- for part in inputMessageHeaders -%}
     {%- if part.element %}
     {{ part.name }} = SCHEMA.get_element_by_name('{{ part.element|remove_namespace }}')
@@ -71,7 +71,7 @@ class {{ inputMessage.name }}Headers(BaseHeader):
 {%- endif -%}
 {%- if outputMessageHeaders %}
 class {{ outputMessage.name }}Headers(BaseHeader):
-    SCHEMA = Schema_{{ schema_name(definitions.types.schema.targetNamespace) }}
+    SCHEMA = Schema_{{ schema_name(definitions.types.schema) }}
     {%- for part in outputMessageHeaders -%}
     {%- if part.element %}
     {{ part.name }} = SCHEMA.get_element_by_name('{{ part.element|remove_namespace }}')
@@ -122,7 +122,7 @@ class {{ outputMessage.name }}Headers(BaseHeader):
     name='{{ port.name }}',
     targetNamespace='{{ definitions.targetNamespace }}',
     location='{{ port.address.location|url_template }}',
-    schema=Schema_{{ schema_name(definitions.types.schema.targetNamespace) }},
+    schema=Schema_{{ schema_name(definitions.types.schema) }},
     {%- if soap_version.NAME == 'soap11' %}
     version=soap.SOAPVersion.SOAP11,
     {%- else %}

soapfish/templates/xsd
@@ -1,6 +1,6 @@
 {#- XSD Imports -#}
 {%- for imp in schema.imports %}
-{{- resolve_import(imp, known_namespaces, schema.targetNamespace, cwd) }}
+{{- resolve_import(imp, known_files, schema.targetNamespace, cwd) }}
 {%- endfor %}
 # {{ schema.targetNamespace }}{# Output the name of the schema in a comment. #}
 {# [blank line] #}
@@ -258,8 +258,8 @@ class {{ element.name|capitalize }}(xsd.ComplexType):
 {% endif %}
 {%- endif %}
 {%- endfor %}
-Schema_{{ schema_name(schema.targetNamespace) }} = xsd.Schema(
-    imports=[{% for i in schema.imports %}Schema_{{ schema_name(i.namespace) }}{% if not loop.last %}, {% endif %}{% endfor %}],
+Schema_{{ schema_name(schema, location) }} = xsd.Schema(
+    imports=[{% for i in schema.imports %}Schema_{{ schema_name(i) }}{% if not loop.last %}, {% endif %}{% endfor %}],
     targetNamespace='{{ schema.targetNamespace }}',
     {%- if location %}
     location='{{ location|url_template }}',{% endif %}

tests/assets/same_namespace/same_namespace.wsdl
@@ -1,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<wsdl:definitions name="TestRelativePaths" targetNamespace="http://example.com" xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <wsdl:types>
+        <xsd:schema targetNamespace="http://example.com">
+            <xsd:import namespace="http://example.com" schemaLocation="schema1.xsd"/>
+            <xsd:import namespace="http://example.com" schemaLocation="schema2.xsd"/>
+        </xsd:schema>
+    </wsdl:types>
+</wsdl:definitions>

tests/assets/same_namespace/schema1.xsd
@@ -1,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsd:schema elementFormDefault="qualified" targetNamespace="http://example.com" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <xsd:element name="Schema1_Element" type="xsd:string"/>
+</xsd:schema>

tests/assets/same_namespace/schema2.xsd
@@ -1,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsd:schema elementFormDefault="qualified" targetNamespace="http://example.com" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <xsd:element name="Schema2_Element" type="xsd:string"/>
+</xsd:schema>

tests/code_generation_test.py
@@ -289,5 +289,15 @@ class CodeGenerationTest(unittest.TestCase):
         assert_contains('Schema2_Element', code)
         assert_contains('Schema3_Element', code)
 
+    def test_import_same_namespace(self):
+        path = 'tests/assets/same_namespace/same_namespace.wsdl'
+        xml = open_document(path)
+        code = generate_code_from_wsdl(xml, 'server', cwd=os.path.dirname(path))
+        if six.PY3:
+            code = code.decode()
+        assert_contains('Schema1_Element', code)
+        assert_contains('Schema2_Element', code)
+
+
 if __name__ == "__main__":
     unittest.main()

tests/xsd_code_generation_test.py
@@ -97,7 +97,7 @@ class XSDCodeGenerationTest(PythonicTestCase):
             '</xs:schema>')
         xml_element = etree.fromstring(xml)
         generated_schema = xsdspec.Schema.parse_xmlelement(xml_element)
-        code_string = xsd2py.schema_to_py(generated_schema, ['xs'], known_namespaces=[])
+        code_string = xsd2py.schema_to_py(generated_schema, ['xs'])
         
         schema, new_symbols = generated_symbols(code_string)
         assert_not_none(schema)
@@ -127,7 +127,7 @@ class XSDCodeGenerationTest(PythonicTestCase):
             '</xs:schema>')
         xml_element = etree.fromstring(xml)
         generated_schema = xsdspec.Schema.parse_xmlelement(xml_element)
-        xsd2py.schema_to_py(generated_schema, ['xs'], known_namespaces=[],
+        xsd2py.schema_to_py(generated_schema, ['xs'],
                             parent_namespace="http://site.example/ws/spec")
 
     def test_can_generate_list_enumeration(self):

commit 0d275b1baf9f62a980a82503a4db7f550821fe38

fix: restore support for remotely hosts files.
Support for various files that could be hosted remotely and included via
elements such as xsd:import, xsd:include or wsdl:import was broken in
the commits 9e9cb6b, a9f6c4c and 0d644b5. This fix attempts to support
both use cases where files are hosted remotely or locally.

License: BSD

soapfish/templates/lib
@@ -14,7 +14,7 @@
 {%- endmacro %}
 
 {% macro render_Schema(schema, location, known_types=None) -%}
-Schema_{{ schema_name(schema, location) }} = xsd.Schema(
+Schema_{{ schema_name(schema) }} = xsd.Schema(
     imports=[{% for i in schema.imports %}Schema_{{ schema_name(i) }}{% if not loop.last %}, {% endif %}{% endfor %}],
     includes=[{% for i in schema.includes %}Schema_{{ schema_name(i) }}{% if not loop.last %}, {% endif %}{% endfor %}],
     targetNamespace='{{ schema.targetNamespace }}',
@@ -27,6 +27,11 @@ Schema_{{ schema_name(schema, location) }} = xsd.Schema(
     complexTypes=[{% for ct in schema.complexTypes %}{{ ct.name|capitalize }}{% if not loop.last %}, {% endif %}{% endfor %}],
     elements={{ '{' }}{% for e in schema.elements %}'{{ e.name }}': xsd.Element({% if e.type %}{{ e.type|type(known_types) }}{% else %}{{ e.name|capitalize }}(){% endif %}){% if not loop.last %}, {% endif %}{% endfor %}{{ '}' }},
 )
+{%- if location %}
+{# [blank line] #}
+# XXX: Workaround for naming issues related to relative locations!
+Schema_{{ schema_name(schema, location) }} = Schema_{{ schema_name(schema) }}
+{%- endif -%}
 {%- endmacro %}
 
 {% macro render_xsd_List(name, known_types=None) -%}

soapfish/utils.py
@@ -79,21 +79,28 @@ def url_template(url):
     return six.moves.urllib.parse.urlunparse(o)
 
 
-def schema_name(schema, location=None):
-    if not location:
-        try:
-            location = schema.schemaLocation
-        except AttributeError:
-            location = schema.targetNamespace
+def schema_name(obj, location=None):
+    from . import xsdspec
+
+    if location:
+        value = location
+    elif isinstance(obj, xsdspec.Schema):
+        value = obj.targetNamespace
+    elif isinstance(obj, xsdspec.Import):
+        value = obj.namespace
+    elif isinstance(obj, xsdspec.Include):
+        value = obj.schemaLocation
+    else:
+        raise TypeError('Unable to generate schema name for %s.%s'
+                        % (obj.__class__.__module__, obj.__class__.__name__))
 
     try:
-        location = location.encode()
+        value = value.encode()
     except UnicodeEncodeError:
         pass
 
-    # we don't have any cryptographic requirements here and md5 is faster than
-    # sha512 so there is no harm using an outdated algorithm.
-    return hashlib.md5(location).hexdigest()[0:5]
+    # no cryptographic requirement here, so use md5 for fast hash:
+    return hashlib.md5(value).hexdigest()[:5]
 
 
 def get_rendering_environment(xsd_namespaces, module='soapfish'):

soapfish/xsd2py.py
@@ -31,21 +31,27 @@ def rewrite_paths(schema, cwd, base_path):
 
     This location is the unique identification for each file, they must match.
     """
-    f = lambda x: os.path.relpath(os.path.normpath(os.path.join(cwd, x.schemaLocation)), base_path)
+    f = lambda x: os.path.relpath(os.path.normpath(os.path.join(cwd, x)), base_path)
     for i in itertools.chain(schema.includes, schema.imports):
-        if i.schemaLocation is None:
+        if i.schemaLocation is None or '://' in i.schemaLocation:
             continue
-        i.schemaLocation = f(i)
+        i.schemaLocation = f(i.schemaLocation)
 
 
-def resolve_import(xsdimport, known_files, parent_namespace, cwd, base_path):
-    location = os.path.join(base_path, xsdimport.schemaLocation)
-    cwd = os.path.dirname(location)
-    logger.info('Generating code for XSD import \'%s\'...' % location)
-    xml = open_document(location)
+def resolve_import(i, known_files, parent_namespace, cwd, base_path):
+    assert isinstance(i, (xsdspec.Import, xsdspec.Include))
+    if '://' in i.schemaLocation:
+        path = location = i.schemaLocation
+        cwd = None
+    else:
+        path = os.path.join(base_path, i.schemaLocation)
+        location = os.path.relpath(path, base_path)
+        cwd = os.path.dirname(path)
+    tag = i.__class__.__name__.lower()
+    logger.info('Generating code for xsd:%s=%s' % (tag, path))
+    xml = open_document(path)
     xmlelement = etree.fromstring(xml)
 
-    location = os.path.relpath(location, base_path)
     return generate_code_from_xsd(xmlelement, known_files, location,
                                   parent_namespace, encoding=None,
                                   cwd=cwd, base_path=base_path)