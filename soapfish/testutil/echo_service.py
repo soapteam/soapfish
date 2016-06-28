ea6f713255e92650ad277c46850f85fe53e18664
Curtis Maloney <curtis@tinbrain.net>
2016-06-28 17:10:20 +1000

    Finish tidying up

--- soapfish/testutil/echo_service.py
+++ soapfish/testutil/echo_service.py
@@ -2,9 +2,6 @@
 
 from __future__ import absolute_import, unicode_literals
 
-from lxml import etree
-from pythonic_testcase import *
-
 from .. import xsd
 from ..core import SOAPResponse
 from ..lib.attribute_dict import AttrDict
@@ -17,6 +14,7 @@ __all__ = [
     'EchoOutputHeader'
 ]
 
+
 class EchoType(xsd.ComplexType):
     INHERITANCE = None
     INDICATOR = xsd.Sequence
@@ -28,29 +26,36 @@ class EchoType(xsd.ComplexType):
         instance.value = value
         return instance
 
+
 def echo_handler():
     state = AttrDict(was_called=False)
+
     def _handler(request, input_):
-        state.update(dict(
-            was_called = True,
-            request = request,
-            input_ = input_,
-            input_header = request.soap_header,
-        ))
+        state.update({
+            'was_called': True,
+            'request': request,
+            'input_': input_,
+            'input_header': request.soap_header,
+        })
         return SOAPResponse(EchoType.create(input_.value))
     return _handler, state
+
 
 class InputVersion(xsd.String):
     pass
 
+
 class OutputVersion(xsd.String):
     pass
+
 
 class EchoInputHeader(xsd.ComplexType):
     InputVersion = xsd.Element(InputVersion)
 
+
 class EchoOutputHeader(xsd.ComplexType):
     OutputVersion = xsd.Element(OutputVersion)
+
 
 def echo_service(handler=None, input_header=None, output_header=None):
     if handler is None:
@@ -69,15 +74,15 @@ def echo_service(handler=None, input_header=None, output_header=None):
         },
     )
     echo_method = xsd.Method(function=handler,
-        soapAction='echo',
-        input='echoRequest',
-        inputPartName='input_',
-        input_header=input_header,
-        output='echoResponse',
-        output_header=output_header,
-        outputPartName='result',
-        operationName='echoOperation',
-    )
+                             soapAction='echo',
+                             input='echoRequest',
+                             inputPartName='input_',
+                             input_header=input_header,
+                             output='echoResponse',
+                             output_header=output_header,
+                             outputPartName='result',
+                             operationName='echoOperation',
+                             )
     return Service(
         name='TestService',
         targetNamespace='http://soap.example/echo',
