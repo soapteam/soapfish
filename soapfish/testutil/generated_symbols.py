b2dd90fc664904f30a2a23505ed3542cb4fa27fe
Oto Skrkal <oto.skrkal@centrum.cz>
2017-04-11 11:04:28 +0200

    templates/lib: fixed wrong default for Attribute's use member when generating code from XSD.

--- soapfish/testutil/generated_symbols.py
+++ soapfish/testutil/generated_symbols.py
@@ -2,11 +2,22 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import contextlib
 import logging
-
+import os
+import random
+import shutil
 import six
+import string
+import sys
+import tempfile
 
-__all__ = ['generated_symbols']
+try:
+    import importlib
+except ImportError:
+    importlib = None
+
+__all__ = ['generated_symbols', 'import_code']
 
 
 def generated_symbols(code):
@@ -18,7 +29,7 @@ def generated_symbols(code):
     try:
         # Let's trust our own code generation...
         six.exec_(code, globals_)
-    except Exception:
+    except Exception as ex:
         logging.warning('Code could not be imported:\n%s', code)
         raise
 
@@ -31,3 +42,28 @@ def generated_symbols(code):
         if isinstance(symbol, xsd.Schema):
             schemas.append(symbol)
     return schemas, symbols
+
+
+@contextlib.contextmanager
+def import_code(code):
+    code_module = None
+    tmp_dir = None
+    try:
+        if tmp_dir is None:
+            tmp_dir = tempfile.mkdtemp()
+        module_name = "import_code_" + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+        with open(os.path.join(tmp_dir, module_name) + ".py", 'w+b') as f:
+            f.write(code)
+        sys.path.append(tmp_dir)
+        if importlib:
+            code_module = importlib.import_module(module_name)
+        else:
+            code_module = __import__(module_name, globals(), {})  # XXX: Python 2.6
+        yield code_module
+    finally:
+        if code_module is not None:
+            del code_module
+        if tmp_dir:
+            if tmp_dir in sys.path:
+                sys.path.remove(tmp_dir)
+            shutil.rmtree(tmp_dir, ignore_errors=True)
---
+++ tests/generation/xsd_attrs_code_test.py
@@ -0,0 +1,84 @@
+from lxml import etree
+from pythonic_testcase import (
+    PythonicTestCase,
+    assert_contains,
+    assert_equals,
+    assert_none,
+    assert_raises
+)
+
+from soapfish import utils, xsd2py
+from soapfish import testutil
+
+
+class ComplexTypeAttributeUsageGenerationTest(PythonicTestCase):
+    def setUp(self):
+        xsd_str = utils.open_document("tests/assets/generation/attribute_usage.xsd")
+        code = xsd2py.generate_code_from_xsd(xsd_str)
+        self.schemas, self.symbols = testutil.generated_symbols(code)
+        self.SampleType = self.symbols["SampleType"]
+
+    def test_can_specify_all_attributes(self):
+        instance = self.SampleType(name="someName", value="someValue", type="someType")
+        element = etree.Element("sample")
+        instance.render(element, instance)
+
+        assert_equals("someName", element.get("name"))
+        assert_equals("someType", element.get("type"))
+        assert_equals("someValue", element.get("value"))
+
+    def test_can_omit_optional_attributes(self):
+        instance = self.SampleType(name="someName")
+        element = etree.Element("sample")
+        instance.render(element, instance)
+
+        assert_equals("someName", element.get("name"))
+        assert_none(element.get("value"))
+        assert_none(element.get("type"))
+
+    def test_cannot_omit_required_attribute(self):
+        instance = self.SampleType(value="someValue", type="someType")
+        element = etree.Element("sample")
+
+        with assert_raises(ValueError) as context:
+            instance.render(element, instance)
+        # the exception raised should mention the field is required
+        assert_contains("required", str(context.caught_exception))
+
+
+class AttributeGroupUsageGenerationTest(PythonicTestCase):
+    def setUp(self):
+        xsd_str = utils.open_document("tests/assets/generation/attrgroup_usage.xsd")
+        self.code = xsd2py.generate_code_from_xsd(xsd_str)
+
+    def test_can_specify_all_attributes(self):
+        with testutil.import_code(self.code) as generated:
+            instance = generated.SampleType(
+                sampleAttrs=generated.SampleAttrs(name="someName", value="someValue", type="someType")
+            )
+            element = etree.Element("sample")
+            instance.render(element, instance)
+
+            assert_equals("someName", element.get("name"))
+            assert_equals("someType", element.get("type"))
+            assert_equals("someValue", element.get("value"))
+
+    def test_can_omit_optional_attributes(self):
+        with testutil.import_code(self.code) as generated:
+            instance = generated.SampleType(sampleAttrs=generated.SampleAttrs(name="someName"))
+            element = etree.Element("sample")
+            instance.render(element, instance)
+
+            assert_equals("someName", element.get("name"))
+            assert_none(element.get("value"))
+            assert_none(element.get("type"))
+
+    def test_cannot_omit_required_attribute(self):
+        with testutil.import_code(self.code) as generated:
+            instance = generated.SampleType(sampleAttrs=generated.SampleAttrs(value="someValue", type="someType"))
+            element = etree.Element("sample")
+
+            with assert_raises(ValueError) as context:
+                instance.render(element, instance)
+            # the exception raised should mention the field is required
+            assert_contains("required", str(context.caught_exception))
