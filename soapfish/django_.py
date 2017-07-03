8ad745d7b808cd3b7a2e74752bc8d8c8eae198f2
Nick Pope <nick.pope@flightdataservices.com>
2017-07-03 13:37:16 +0100

    django: fix header name conversion.

--- soapfish/django_.py
+++ soapfish/django_.py
@@ -14,7 +14,7 @@ class DjangoEnvironWrapper(object):
         self.environ = environ
 
     def get(self, name, default=None):
-        cgi_name = 'HTTP_' + name
+        cgi_name = 'HTTP_' + name.replace('-', '_').upper()
         for key in (name, cgi_name):
             if key in self.environ:
                 return self.environ[key]
