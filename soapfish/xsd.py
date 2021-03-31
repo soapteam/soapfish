soapteam/soapfish

commit 2fb09d254d935fcf4386f585934dcc06900bcb40

Fixed py/missing-equals alert in LGTM.
See https://lgtm.com/rules/9990086/ for details.

soapfish/xsd.py
@@ -765,6 +765,11 @@ class TypedList(list):
             raise ValueError(f'You must not add more than {self._list._maxOccurs} items to this list.')
         super().append(accepted_value)
 
+    def __eq__(self, other):
+        if not isinstance(other, TypedList):
+            return False
+        return self._list == other._list and super().__eq__(other)
+
 
 class ListElement(Element):
     """
@@ -1195,3 +1200,8 @@ class NamedType(ComplexType):
     def __init__(self, name=None, value=None):
         self.name = name
         self.value = value
+
+    def __eq__(self, other):
+        if not isinstance(other, NamedType):
+            return False
+        return self.name == other.name and self.value == other.value and super().__eq__(other)

commit d52cc377366f9133f986b5edbaa32d895915fb54

Fixed regression in 2fb09d254d935fcf4386f585934dcc06900bcb40.

soapfish/xsd.py
@@ -748,7 +748,7 @@ class Content(Ref):
         return None
 
 
-class TypedList(list):
+class TypedList(list):  # lgtm [py/missing-equals]
     def __init__(self, element):
         super().__init__()
         self._list = element
@@ -764,11 +764,6 @@ class TypedList(list):
         if self._list._maxOccurs is not None and (len(self) + 1 > self._list._maxOccurs):
             raise ValueError(f'You must not add more than {self._list._maxOccurs} items to this list.')
         super().append(accepted_value)
-
-    def __eq__(self, other):
-        if not isinstance(other, TypedList):
-            return False
-        return self._list == other._list and super().__eq__(other)
 
 
 class ListElement(Element):