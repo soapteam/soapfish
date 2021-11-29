03e12dca3c90596ccc53e80f68bb50882382a45f
Nick Pope <nick@nickpope.me.uk>
2021-11-29 22:02:10 +0000

    Fix linting issues.

--- tests/soapfish_response_test.py
+++ tests/soapfish_response_test.py
@@ -3,10 +3,7 @@ import unittest
 from soapfish.core import SOAPResponse
 
 
-
 class SOAPResponseHandling(unittest.TestCase):
     def test_constructor(self):
         response = SOAPResponse('', http_status_code=500)
         self.assertEqual(500, response.http_status_code)
-
-
